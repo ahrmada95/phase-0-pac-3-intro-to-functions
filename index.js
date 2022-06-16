e025bfb6b7c3c1226d4a1a25303b6989f4518a46
Ahnaf Ahmad <ahrmada95@gmail.com>
2022-06-16 14:16:22 -0400

    Finished code-along

--- index.js
+++ index.js
@@ -1,1 +1,64 @@
 // Follow along with the examples here
+
+function say(greeting, firstName) {
+    return `${greeting}, ${firstName}!`;
+  }
+
+console.log(say("hello", "ahnaf"));
+/*
+function add(x, y) {
+    return x + y;
+  }
+
+console.log(add(1, 2));
+*/
+
+/*
+function say(greeting, firstName) {
+    console.log(`${greeting}, ${firstName}!`);
+  }
+
+say("Hello", "Ahnaf");
+*/
+
+/*
+function sayHelloToGuadalupe() {
+    console.log("Hello, Guadalupe!");
+  }
+
+function sayHelloToLiz() {
+  console.log("Hello, Liz!");
+}
+
+function sayHelloToSamip() {
+    console.log("Hello, Samip!");
+  }
+
+sayHelloToGuadalupe();
+sayHelloToLiz();
+sayHelloToSamip();
+*/
+
+/*
+function doNothing() {}
+
+function sayHello() {
+    console.log("Hello!");
+  }
+
+sayHello();
+*/
+
+/*
+console.log("Hello, world!");
+console.log("Hello, world!");
+console.log("Hello, world!");
+console.log("Hello, world!");
+console.log("Hello, world!");
+console.log("Hello, world!");
+console.log("Hello, world!");
+console.log("Hello, world!");
+console.log("Hello, world!");
+console.log("Hello, world!");
+console.log("Hello, world!");
+*/
